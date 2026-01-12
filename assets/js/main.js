8d5f5fa9c413a90c1099fc9222bea0a8beeb1a49
Mofoletta <sofija.daniilova@gmail.com>
2026-01-12 18:36:55 +0200

    feat: clickable lessons list with lesson detail screen and navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,6 +29,12 @@ function showLessons() {
     for (let i = 1; i <= 18; i += 1) {
       const li = document.createElement('li');
       li.textContent = `Урок ${i}`;
+      li.tabIndex = 0;
+      li.style.cursor = 'pointer';
+      li.addEventListener('click', () => showLesson(i));
+      li.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter' || e.key === ' ') showLesson(i);
+      });
       list.appendChild(li);
     }
   }
@@ -37,6 +43,28 @@ function showLessons() {
 
 function goBackToMenu() {
   showScreen('menuScreen');
+}
+
+function goBackToLessons() {
+  showScreen('lessonsScreen');
+}
+
+function showLesson(num) {
+  const titleEl = document.getElementById('lessonTitle');
+  const bodyEl = document.getElementById('lessonBody');
+  if (titleEl && bodyEl) {
+    titleEl.textContent = `Урок ${num}`;
+    bodyEl.innerHTML = `
+      <p><strong>Тема:</strong> Введение в тему урока ${num}.</p>
+      <p>Это демонстрационный текст содержания. Здесь позже можно подставить настоящий материал, изображения, ссылки и задания.</p>
+      <ol>
+        <li>Пункт 1 содержания урока ${num}</li>
+        <li>Пункт 2 содержания</li>
+        <li>Пункт 3 содержания</li>
+      </ol>
+    `;
+  }
+  showScreen('lessonScreen');
 }
 
 function showGame() {
@@ -50,3 +78,5 @@ window.showConstitution = showConstitution;
 window.showLessons = showLessons;
 window.showGame = showGame;
 window.goBackToMenu = goBackToMenu;
+window.goBackToLessons = goBackToLessons;
+window.showLesson = showLesson;
